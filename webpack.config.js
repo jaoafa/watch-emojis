3453831b4757bf5c09abf680376c268680ca0a39
Tomachi [ICHIGO] <lego@aol.jp>
2021-10-21 05:07:29 +0900

    fix: webpack動作修正

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./src/main.ts",
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "main.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    }
+};
