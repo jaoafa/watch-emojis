8f95c1fbf1699a36bc625ec18e217ec3cc4fc5dd
Tomachi [ICHIGO] <lego@aol.jp>
2021-10-21 06:16:07 +0900

    feat: とりあえずping pongできるところまで

---
+++ README.md
@@ -0,0 +1,5 @@
+# watch-emojis
+
+## Requirements
+
+- Node.js `v16` (Tested with `v16.12.0`)
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log('Hello World')
+import config from 'config'
+import { Client, Intents, TextChannel } from 'discord.js'
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+})
+
+client.on('ready', () => {
+  console.log(`I am ready! ${client.user?.tag}`)
+})
+
+client.on('messageCreate', async (message) => {
+  if (!(message.channel instanceof TextChannel)) {
+    return
+  }
+  if (message.content === 'ping') {
+    message.reply('pong')
+  }
+})
+
+client.login(config.get('discordToken'))
